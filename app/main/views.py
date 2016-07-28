31f696c0e973e9beec262d2cd7c1da2d282663cd
wangbicong <wangbicong@hotmail.com>
2016-07-28 17:51:42 +0800

    表单实例

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, redirect, session, url_for, flash
 from . import main_blueprint
+from forms import NameForm
 
 
-@main_blueprint.route('/')
+@main_blueprint.route('/', methods=['get', 'post'])
 def index():
-    return render_template('main.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name and old_name != form.name.data:
+            flash('You changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('main.index'))
+    return render_template('main.html', form=form, name=session.get('name'))
